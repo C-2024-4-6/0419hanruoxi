0e09dd52e1525e0a77417e52ca54953f4a6e4a1e
HanXxi <3544159204@qq.com>
2024-11-05 21:23:46 +0800

    实验2.1

---
+++ 实验2.1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	char ch;
+	cout << "ÇëÊäÈëÒ»¸ö×Ö·û£º" << endl;
+	cin >> ch;
+	if (ch >= 'a' && ch <= 'z')
+	{
+		cout << char(ch - 32) << endl;
+	}
+	else
+	{
+		cout << char(ch+1) << endl;
+	}
+	return 0;
+}
